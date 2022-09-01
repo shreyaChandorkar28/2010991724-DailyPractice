91b1ab4517506b1157a27d922ab9c991c74c2a46
Shreya Chandorkar <shreya1724.be20@chitkara.edu.in>
2022-09-01 15:01:33 +0530

    26 August 2022

---
+++ 26August2022/myapp/src/component/Header.js
@@ -0,0 +1,11 @@
+import App from "../App";
+
+// Uppercase of Header.js's H is naming convention
+const Header = () => {
+    return (
+    <header>
+    <h1>Hello Header</h1>
+    </header>
+    );
+    };
+    export default Header;
