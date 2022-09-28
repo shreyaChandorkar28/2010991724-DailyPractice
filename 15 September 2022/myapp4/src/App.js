e294d1ff138c869fa5ea03ed82d1c5ab8e34ebc4
Shreya Chandorkar <shreya1724.be20@chitkara.edu.in>
2022-09-28 21:44:37 +0530

    React

--- 15 September 2022/myapp4/src/App.js
+++ 15 September 2022/myapp4/src/App.js
@@ -1,11 +1,23 @@
+import React, {Component} from  'react';
 
-import './App.css';
-import Form from './Form.js';
+
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+//import './App.css';
+import Home from './pages/Home.js';
+import About from './pages/About.js';
+
+
+
 function App() {
   return (
-    <div className="App">
-      <Form />
-    </div>
+    <Router>
+   <div>
+<Route exact path="/" component = {Home} />
+<Route exact path="/About" component = {About} />
+
+
+   </div>
+   </Router>
   );
 }
 
